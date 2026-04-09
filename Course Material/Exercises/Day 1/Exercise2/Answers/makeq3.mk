3d4717196c0b441328497f1fe8915d1e10c41771
Sabarish Sridhar <sabarish.sridhar@afry.com>
2026-04-09 11:15:02 +0200

    Fixes for exericse 2 and 3

--- Course Material/Exercises/Day 1/Exercise2/Answers/makeq3.mk
+++ Course Material/Exercises/Day 1/Exercise2/Answers/makeq3.mk
@@ -1,21 +1,21 @@
 SHELL := /bin/bash
 
 # Task 1:
-# Populate a target called build-images with an @echo "Saying it is building the image"
-# Populate the build-image with and @echo "cd ~/work/poky/poky/build && source oe-init-build-env && bitbake core-image-minimal"
+# Populate a target called build-core-image with an @echo "Saying it is building the image"
+# Populate the build-image with and @echo "cd ~/git/poky/ && source oe-init-build-env && bitbake core-image-minimal"
 
 
 # Task 2:
 # Create a target called clean-build
 # Populate the clean-build with an @echo "Saying it is cleaning the build directory"
-# Populate the clean-build with and @echo "cd ~/work/poky/poky/build && source oe-init-build-env && bitbake -c cleanall"
+# Populate the clean-build with and @echo "cd ~/git/poky/ && source oe-init-build-env && bitbake -c cleanall"
 
 # Targets
-build-image: #Leave this line empty
-@echo "Saying it is cleaning the build directory"
-@echo "cd ~/work/poky/poky && source oe-init-build-env && bitbake core-image-minimal"
+build-core-image: #Leave this line empty
+echo "Building the core-iamge-minimal image"
+cd ~/git/poky && source oe-init-build-env && bitbake core-image-minimal
 
 clean-build:
-@echo "Saying it is cleaning the build directory"
-@echo "cd ~/work/poky/poky/ && source oe-init-build-env && bitbake -c cleanall"
+echo "Cleaning the build directory"
+@echo "cd ~/git/poky/ && source oe-init-build-env && bitbake -c cleanall"
 
